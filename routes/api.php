8ad1b6f8b4e31bffb1b9c57b37b2c1e1462578d1
longth305 <long.th@anlab.pro>
2022-04-13 18:00:32 +0700

    u

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+// api.php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/post/create', 'PostController@store');
+Route::get('/post/edit/{id}', 'PostController@edit');
+Route::post('/post/update/{id}', 'PostController@update');
+Route::delete('/post/delete/{id}', 'PostController@delete');
+Route::get('/posts', 'PostController@index');
